ea151ce0479aef2c24e6a2d77b384ce68407d9a5
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-03-28 23:26:26 +0200

    group route

--- laravel13/routes/web.php
+++ laravel13/routes/web.php
@@ -4,20 +4,22 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('worker.index');
 });
 
 Route::get('/index', function () {
     return view('worker.index');
 })->name('index')->middleware('auth');
 
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
 
-Route::post('/index', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/register', 'showRegister')->name('show.register');
+    Route::get('/login', 'showLogin')->name('show.login');
+    Route::post('/register', 'register')->name('register');
+    Route::post('/login', 'login')->name('login');
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
 Route::resource('clients', 'App\Http\Controllers\ClientController');
