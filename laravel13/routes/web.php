a289ccbff1a53fb107a3e6d4787f226cd2ea3a64
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-03-28 20:33:34 +0200

    Clients controller

--- laravel13/routes/web.php
+++ laravel13/routes/web.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('clients', 'App\Http\Controllers\ClientController');
 Route::resource('students', 'App\Http\Controllers\StudentController');
