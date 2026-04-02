757578bbe45f4e44faeb2c89a43a315e55af3407
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-02 15:22:32 +0300

    Update dashboard.blade.php

--- laravel13/resources/views/clients/dashboard.blade.php
+++ laravel13/resources/views/clients/dashboard.blade.php
@@ -2,5 +2,19 @@
 @extends('layout')
 
 @section('content')
-<p>{{$client->firstName}}</p>
+<p><strong>Name:</strong> {{ $client->user->firstName }} {{ $client->user->lastName }}</p>
+<p><strong>Email:</strong> {{ $client->user->email }}</p>
+<p><strong>EGN:</strong> {{ $client->clientEgn }}</p>
+<p><strong>Accounts:</strong> {{ $client->bankAccounts->count() }}</p>
+<p><strong>Total Value:</strong> {{ $client->bankAccounts->sum('balance') }}</p>
+
+<h3>Bank Accounts</h3>
+
+<ul>
+    @foreach($client->bankAccounts as $account)
+    <li>
+        {{ $account->bankAccountNumber }} | Balance: {{ $account->balance }}
+    </li>
+    @endforeach
+</ul>
 @endsection
